5d1ef56d75409f5c105cce67c0202a727d85244e
alinarogova123-arch <alinarogova123@gmail.com>
2026-09-15 14:39:25 +0300

    Save messages history to file.

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 import asyncio
+import datetime
+import aiofiles
+import socket
+import sys
+
+
+TIMEOUT = 10
+
 
 async def tcp_echo_client(message):
-    reader, writer = await asyncio.open_connection(
-        'minechat.dvmn.org', 5000)
-
-    # print(f'Send: {message!r}')
-    # writer.write(message.encode())
-    # await writer.drain()
-    
     while True:
-        data = await reader.readuntil(b'\n')
-        print(data.decode())
+        try:
+            reader, writer = await asyncio.open_connection(
+                'minechat.dvmn.org', 5000)
+        
+        except socket.gaierror:
+            await asyncio.sleep(TIMEOUT)
+            continue
 
-    # print('Close the connection')
-    # writer.close()
-    # await writer.wait_closed()
+            now = datetime.datetime.now().strftime("%d.%m.%Y %H:%M")
+            async with aiofiles.open("test.txt", "a") as file:
+                await file.write(f"[{now}] Установлено соединение\n")
+            
+        try:
+            while True:
+                data = await asyncio.wait_for(reader.readuntil(b'\n'), timeout=TIMEOUT)
+                if not data:
+                    break
+                now = datetime.datetime.now().strftime("%d.%m.%Y %H:%M")
+                async with aiofiles.open("test.txt", "a") as file:
+                    await file.write(f"[{now}] {data.decode().strip()}")
+                print(data.decode())
+        
+        except TimeoutError:
+            if writer:
+                writer.close()
+                await writer.wait_closed()
 
-asyncio.run(tcp_echo_client('Hello World!'))
+try:
+    asyncio.run(tcp_echo_client('Hello World!'))
+except KeyboardInterrupt:
+    sys.exit(0)
