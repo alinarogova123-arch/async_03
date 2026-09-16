ed9f84d1745c2fa50514982641bf87961e6ef69f
alinarogova123-arch <alinarogova123@gmail.com>
2026-09-16 11:33:16 +0300

    Add CLI options.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import datetime
+import argparse
 import aiofiles
 import socket
 import sys
@@ -8,19 +9,29 @@ import sys
 TIMEOUT = 10
 
 
-async def tcp_echo_client(message):
+def create_parcer():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--host', type=str, default='minechat.dvmn.org')
+    parser.add_argument('--port', type=int, default=5000)
+    parser.add_argument('--history', type=str, default="chat.txt")
+    
+    return parser.parse_args()
+
+
+async def tcp_echo_client(host, port, history):
     while True:
         try:
             reader, writer = await asyncio.open_connection(
-                'minechat.dvmn.org', 5000)
+                host, port)
         
         except socket.gaierror:
             await asyncio.sleep(TIMEOUT)
             continue
 
-            now = datetime.datetime.now().strftime("%d.%m.%Y %H:%M")
-            async with aiofiles.open("test.txt", "a") as file:
-                await file.write(f"[{now}] Установлено соединение\n")
+        now = datetime.datetime.now().strftime("%d.%m.%Y %H:%M")
+        async with aiofiles.open(history, "a") as file:
+            await file.write(f"[{now}] !!! Установлено соединение !!!\n")
             
         try:
             while True:
@@ -28,16 +39,22 @@ async def tcp_echo_client(message):
                 if not data:
                     break
                 now = datetime.datetime.now().strftime("%d.%m.%Y %H:%M")
-                async with aiofiles.open("test.txt", "a") as file:
-                    await file.write(f"[{now}] {data.decode().strip()}")
-                print(data.decode())
+                async with aiofiles.open(history, "a") as file:
+                    await file.write(f"[{now}] {data.decode()}")
+                print(data.decode().strip())
         
         except TimeoutError:
-            if writer:
-                writer.close()
-                await writer.wait_closed()
+            writer.close()
+            await writer.wait_closed()
 
-try:
-    asyncio.run(tcp_echo_client('Hello World!'))
-except KeyboardInterrupt:
-    sys.exit(0)
+
+async def main():
+    args = create_parcer()
+    await tcp_echo_client(args.host, args.port, args.history)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        sys.exit(0)
