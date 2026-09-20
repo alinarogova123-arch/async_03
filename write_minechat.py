809de1b4246b31d140f71787d1d3fd43d22c53ad
alinarogova123-arch <alinarogova123@gmail.com>
2026-09-20 22:43:53 +0300

    Escape control symbols.

--- write_minechat.py
+++ write_minechat.py
@@ -87,54 +87,11 @@ async def submit_message(writer, message):
     logger.debug(f'DEBUG:Send: {message!r}')
 
 
-# async def tcp_echo_client(key, host, port, token, nickname="", registration=False):
-#     message = "123"
-#     reader, writer = await asyncio.open_connection(
-#         host, port)
-
-#     data = await reader.readuntil(b'\n')
-#     logger.debug(f'DEBUG:Received: {data.decode()!r}')
-
-#     writer.write(f"{token}\n".encode())
-#     await writer.drain()
-#     logger.debug(f'DEBUG:Send: {token!r}')
-
-#     if registration:
-#         data = await reader.readuntil(b'\n')
-#         logger.debug(f'DEBUG:Received: {data.decode()!r}')
-#         writer.write(f"{nickname}\n".encode())
-#         logger.debug(f'DEBUG:Send: {nickname!r}')
-
-#     data = await reader.readuntil(b'\n')
-#     data = json.loads(data.decode())
-    
-#     if not data:
-#         print("Неизвестный токен. Проверьте его или зарегистрируйте заново.")
-#         return
-
-#     if registration:
-#         logger.debug(f'DEBUG:Received: {data}')
-#         async with aiofiles.open(key, "w") as file:
-#             await file.write(data.get("account_hash"))
-#         return
-
-#     logger.debug(f'DEBUG:Received: {data}')
-
-#     data = await reader.readuntil(b'\n')
-#     logger.debug(f'DEBUG:Received: {data.decode()!r}')
-
-#     writer.write(f"{message}\n\n".encode())
-#     await writer.drain()
-#     logger.debug(f'DEBUG:Send: {message!r}')
-
-#     writer.close()
-#     await writer.wait_closed()
-
-
 async def main():
     args = create_parcer()
     if not os.path.exists(args.key):
-        await register(args.key, args.host, args.port, args.nickname)
+        nickname = args.nickname.replace("\n", " ")
+        await register(args.key, args.host, args.port, nickname)
     
     async with aiofiles.open(args.key, "r", encoding="utf-8") as file:
         token = await file.read()
@@ -144,7 +101,8 @@ async def main():
         print("Неизвестный токен. Проверьте его или зарегистрируйте заново.")
         return
 
-    await submit_message(writer, args.message)
+    message = args.message.replace("\n", " ")
+    await submit_message(writer, message)
 
     writer.close()
     await writer.wait_closed()
