af9b12b4fc15b1db7860d50c713260c9cb0ba1ca
alinarogova123-arch <alinarogova123@gmail.com>
2026-09-21 12:49:08 +0300

    Add CLI options to sender script.

--- write_minechat.py
+++ write_minechat.py
@@ -7,6 +7,9 @@ import os
 import aiofiles
 
 
+TOKEN_KEY = "token.txt"
+
+
 class MyLogsHandler(logging.Handler):
     def emit(self, record):
         log_entry = self.format(record)
@@ -18,27 +21,58 @@ logger.addHandler(MyLogsHandler())
 
 
 def create_parcer():
-    parser = argparse.ArgumentParser()
-    
-    parser.add_argument('--host', type=str, default='minechat.dvmn.org')
-    parser.add_argument('--nickname', type=str, default='')
-    parser.add_argument('--port', type=int, default=5050)
-    parser.add_argument('--key', type=str, default="token.txt")
-    parser.add_argument('--message', type=str, default="Hello world")
+    parser = argparse.ArgumentParser(
+                 prog='write_minechat',
+                 description='Скрипт отправки сообщения в чат',
+             )    
+    parser.add_argument(
+        '--host',
+        type=str,
+        default='minechat.dvmn.org',
+        help="Имя хоста",
+    )
+    parser.add_argument(
+        '-n',
+        '--nickname',
+        type=str,
+        default='',
+        help="Псевдоним пользователя",
+    )
+    parser.add_argument(
+        '-p',
+        '--port',
+        type=int,
+        default=5050,
+        help="Номер порта",
+    )
+    parser.add_argument(
+        '-t',
+        '--token',
+        type=str,
+        default="",
+        help="Уникальный токен пользователя",
+    )
+    parser.add_argument(
+        '-m',
+        '--message',
+        required=True,
+        type=str,
+        help="Сообщение",
+    )
     
     return parser.parse_args()
 
 
-async def register(key, host, port, nickname):
+async def register(TOKEN_KEY, host, port, nickname):
     reader, writer = await asyncio.open_connection(
         host, port)
 
     data = await reader.readuntil(b'\n')
     logger.debug(f'DEBUG:Received: {data.decode()!r}')
 
-    writer.write(f"\n".encode())
+    writer.write("\n".encode())
     await writer.drain()
-    logger.debug(f'DEBUG:Send: ')
+    logger.debug('DEBUG:Send: ')
 
     data = await reader.readuntil(b'\n')
     logger.debug(f'DEBUG:Received: {data.decode()!r}')
@@ -50,7 +84,7 @@ async def register(key, host, port, nickname):
     data = json.loads(data.decode())
     logger.debug(f'DEBUG:Received: {data}')
 
-    async with aiofiles.open(key, "w") as file:
+    async with aiofiles.open(TOKEN_KEY, "w") as file:
         await file.write(data.get("account_hash"))
 
 
@@ -89,16 +123,18 @@ async def submit_message(writer, message):
 
 async def main():
     args = create_parcer()
-    if not os.path.exists(args.key):
+    token = args.token
+    if not os.path.exists(TOKEN_KEY) and not token:
         nickname = args.nickname.replace("\n", " ")
-        await register(args.key, args.host, args.port, nickname)
+        await register(TOKEN_KEY, args.host, args.port, nickname)
     
-    async with aiofiles.open(args.key, "r", encoding="utf-8") as file:
-        token = await file.read()
+    if not token:
+        async with aiofiles.open(TOKEN_KEY, "r", encoding="utf-8") as file:
+            token = await file.read()
     
     writer = await authorise(args.host, args.port, token)
     if not writer:
-        print("Неизвестный токен. Проверьте его или зарегистрируйте заново.")
+        logger.info("Неизвестный токен. Проверьте его или зарегистрируйте заново.")
         return
 
     message = args.message.replace("\n", " ")
